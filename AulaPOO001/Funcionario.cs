0b3e4cb40d70fca5a5121cde1c9b62c03759ddf3
Alexandre de Oliveira <alexmontanha@hotmail.com>
2022-11-13 09:37:24 -0300

    Adicionar arquivos de projeto.

---
+++ AulaPOO001/Funcionario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaPOO001
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public int Id { get; set; }
+        public double SalarioHora { get; set; }
+        public virtual double HorasMes()
+        {
+            int DiasUteis = 20;
+            int HorasDia = 8;
+            return DiasUteis * HorasDia;
+        }
+
+        public double SalarioMensal()
+        {
+            return HorasMes() * SalarioHora;
+        }
+    }
+
+    public class Professor: Funcionario
+    {
+        public double HorasSemanais { get; set; }
+
+        public override double HorasMes()
+        {
+            double horasMes = HorasSemanais * 4.5;
+            return horasMes;
+        }
+    }
+}
---
+++ AulaPOO001/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaPOO001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            funcionario.Nome = "Juka";
+            funcionario.SalarioHora = 25;
+
+            Console.WriteLine($"Nome: {funcionario.Nome}, Salário Hora: {funcionario.SalarioHora}, Salário Mensal: {funcionario.SalarioMensal()}, Horas Trabalhadas Mês: {funcionario.HorasMes()}");
+
+            Professor professor = new Professor();
+            professor.Nome = "Montanha";
+            professor.HorasSemanais = 6;
+            professor.SalarioHora = 25;
+
+            Console.WriteLine($"Nome: {professor.Nome}, Salário Hora: {professor.SalarioHora}, Salário Mensal: {professor.SalarioMensal()}, Horas Trabalhadas Mês: {professor.HorasMes()}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
